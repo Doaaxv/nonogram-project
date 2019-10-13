0eabb3f8256553ef5b3ded04c19f88f62359dc29
Doaa AlHajjaji <Doaa.mh.uni@gmail.com>
2019-10-14 00:04:51 +0300

    fixed horizontal heading and added new patterns

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,122 +5,234 @@ table.setAttribute("id", "table")
 
 let counter = 0
 var arr = []
-let arrThHorizontal = ["", " ", 2, 4, 4, 4, 2]
-let arrThVertical = ["1,1", "5", "5", "3", "1"]
-
-//make a the table head
-let thead_horizontal = document.createElement("thead")
-let tr_horizontal = document.createElement("tr")
-for (let a = 0; a < arrThHorizontal.length; a++) {
-    let th_horizontal = document.createElement("th");
-    th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
-    th_horizontal.setAttribute("class", "thHorizontal")
-    th_horizontal.innerText = arrThHorizontal[a]
-    tr_horizontal.appendChild(th_horizontal)
-}
-thead_horizontal.appendChild(tr_horizontal)
-table.appendChild(thead_horizontal)
+//heart
+// var arrThHorizontal = ["2,1", "4", "4", "4", "2"]
+// var arrThVertical = ["1,1,1", "5", "5", "3", "1"]
+// var rows = 5
+//arrow
+// var arrThHorizontal = [ "1","1","5","1","1"]
+// var arrThVertical = ["1","3","1,1,1,","1","1"]
+// var rows = 5
+//umbrella
+var arrThHorizontal = ["1","2","2,1","7","2","2","1"]
+var arrThVertical = ["1","5","7","1","1","1","2"]
+var rows = 7
 
 
-var long_element = " " 
-
-
-for (let i = 0; i < arrThVertical.length; i++) {
-    console.log(arrThVertical[i].length)
-    if (arrThVertical[i].length > long_element.length) {
-        console.log(" HERE")
-        long_element = arrThVertical[i]
-        long_element_length = arrThVertical[i].length
+function longestElement(array) {
+    var element = " "
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > element.length) {
+            element = array[i]
+        }
     }
+
+    return element
+}
+
+//vertical 
+var longest_vertical_element = longestElement(arrThVertical)
+let longest_element_vertical = longest_vertical_element.split(",")
+
+//horizontal
+var longest_horizontal_element = longestElement(arrThHorizontal)
+let longest_element_horizontal = longest_horizontal_element.split(",")
+
+if(longest_element_vertical!=0){
+for (let i = 0; i < longest_element_vertical.length; i++) {
+    arrThHorizontal.unshift(" ")
+}}else{
+    arrThHorizontal.unshift(" ") 
 }
 
 
-let longest_element = long_element.split(",")
-console.log("lon_el_size" + longest_element.length)
+var cols = 1
+if (longest_element_horizontal != 0) {
+    cols = longest_element_horizontal.length
+}
+
+
+
+
+if(cols != 1){
+for (let i = 0; i < cols; i++) {
+    let thead_horizontal = document.createElement("thead")
+    let tr_horizontal = document.createElement("tr")
+    for (let a = 0; a < arrThHorizontal.length; a++) {
+        if (arrThHorizontal[a].length > 1) {
+            var bigOne = arrThHorizontal[a].split(",")
+            var difference_hori = 0
+            if (bigOne.length < cols) {
+                difference_hori = cols - bigOne.length
+                if (i >= difference_hori) {
+                    let th_horizontal = document.createElement("th");
+                    th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
+                    th_horizontal.setAttribute("class", "thHorizontal")
+                    th_horizontal.innerText = bigOne[i - 1]
+                    tr_horizontal.appendChild(th_horizontal)
+                } else {
+                    let th_horizontal = document.createElement("th");
+                    th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
+                    th_horizontal.setAttribute("class", "thHorizontal")
+                    th_horizontal.innerText = " "
+                    tr_horizontal.appendChild(th_horizontal)
+                }
+
+            } else {
+                let th_horizontal = document.createElement("th");
+                th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
+                th_horizontal.setAttribute("class", "thHorizontal")
+                th_horizontal.innerText = bigOne[i]
+                tr_horizontal.appendChild(th_horizontal)
+            }
+
+
+        } else {
+            if (i > 0) {
+                let th_horizontal = document.createElement("th");
+                th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
+                th_horizontal.setAttribute("class", "thHorizontal")
+                th_horizontal.innerText = arrThHorizontal[a]
+                tr_horizontal.appendChild(th_horizontal)
+            } else {
+                let th_horizontal = document.createElement("th");
+                th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
+                th_horizontal.setAttribute("class", "thHorizontal")
+                th_horizontal.innerText = " "
+                tr_horizontal.appendChild(th_horizontal)
+
+            }
+        }
+
+    }
+    
+    thead_horizontal.appendChild(tr_horizontal)
+    table.appendChild(thead_horizontal)
+}
+}else{
+    let thead_horizontal = document.createElement("thead")
+    let tr_horizontal = document.createElement("tr")
+    for (let a = 0; a < arrThHorizontal.length; a++) {
+        let th_horizontal = document.createElement("th");
+        th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
+        th_horizontal.setAttribute("class", "thHorizontal")
+        th_horizontal.innerText = arrThHorizontal[a]
+        tr_horizontal.appendChild(th_horizontal) 
+    }
+    thead_horizontal.appendChild(tr_horizontal)
+    table.appendChild(thead_horizontal)
+}
+
+
+
+
+
 
 function createVerticalHeading(i, col, txt, thVert) {
-    if (col == 0) {
-        thVert.setAttribute("id", "thV" + arrThVertical[i])
-        thVert.setAttribute("class", "verticalth")
-        thVert.innerText = txt
-    } else {
-        thVert.setAttribute("id", "thV" + arrThVertical[i])
-        thVert.setAttribute("class", "verticalth")
-        thVert.innerText = txt
-    }
+    // if (col == 0) {
+    thVert.setAttribute("id", "thV" + arrThVertical[i])
+    thVert.setAttribute("class", "verticalth")
+    thVert.innerText = txt
+    // } else {
+    //     thVert.setAttribute("id", "thV" + arrThVertical[i])
+    //     thVert.setAttribute("class", "verticalth")
+    //     thVert.innerText = txt
+    // }
 
 
 }
 
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < rows; i++) {
     let row = document.createElement("tr");
     row.setAttribute("id", "row" + i);
     let el_length = arrThVertical[i].split(",");
-    let el_len = 0
-    
-    if (longest_element != 0) {
-        for (let z = 0; z < longest_element.length; z++) {
-            if (arrThVertical[i].length > 1) {
-                if (el_length.length < longest_element.length) {
-                    if (z == 0) {
+
+    if (longest_element_vertical != 0) {
+        if (arrThVertical[i].length > 1) {
+
+            let forLength = longest_element_vertical.length
+
+            var diffrence = 0
+            if (el_length.length < longest_element_vertical.length) {
+                diffrence = longest_element_vertical.length - el_length.length
+
+            }
+            for (let z = 0; z < forLength; z++) {
+                if (diffrence != 0) {
+                    if (z >= diffrence) {
                         let thVert = document.createElement("th");
-                        createVerticalHeading(i, 0, " ", thVert)
+                        createVerticalHeading(i, 0, el_length[z - 1], thVert)
                         row.appendChild(thVert)
                     } else {
                         let thVert = document.createElement("th");
-                        createVerticalHeading(i, 0, el_length[el_len], thVert)
+                        createVerticalHeading(i, 0, " ", thVert)
                         row.appendChild(thVert)
-                        el_len++
                     }
+
                 } else {
+
                     let thVert = document.createElement("th");
                     createVerticalHeading(i, 0, el_length[z], thVert)
                     row.appendChild(thVert)
                 }
-            } else {
-                if (z == longest_element.length - 1) {
-                    let thVert = document.createElement("th");
-                    createVerticalHeading(i, longest_element, arrThVertical[i], thVert)
-                    row.appendChild(thVert)
-                } else {
-                    let thVert = document.createElement("th");
-                    createVerticalHeading(i, longest_element, " ", thVert)
-                    row.appendChild(thVert)
-                }
             }
+        } else {
+            for (let z = 0; z < longest_element_vertical.length - 1; z++) {
+                let thVert = document.createElement("th");
+                createVerticalHeading(i, 0, " ", thVert)
+                row.appendChild(thVert)
+            }
+            let thVert = document.createElement("th");
+            createVerticalHeading(i, longest_element_vertical.length, arrThVertical[i], thVert)
+            row.appendChild(thVert)
 
         }
-    } else {
-        let thVert = document.createElement("th");
-        createVerticalHeading(i, longest_element.length, arrThVertical[i], thVert)
-        row.appendChild(thVert)
+
+        for (let j = 0; j < rows; j++) {
+            let data = document.createElement("td");
+            data.setAttribute("id", i + "x" + j);
+            let obj2 = { "id": i + "x" + j, "checkTest": 0 }
+            data.innerText = i + "x" + j
+            arr.push(obj2)
+            // data.innerText="row"+i+"x"+j
+            data.addEventListener("click", checkTd);
+
+
+            row.appendChild(data);
+
+        }
+
+
+        table.appendChild(row);
+
+    }else{
+
+            let thVert = document.createElement("th");
+            createVerticalHeading(i, longest_element_vertical.length, arrThVertical[i], thVert)
+            row.appendChild(thVert)
+        
+
+
+        for (let j = 0; j < rows; j++) {
+            let data = document.createElement("td");
+            data.setAttribute("id", i + "x" + j);
+            let obj2 = { "id": i + "x" + j, "checkTest": 0 }
+            data.innerText = i + "x" + j
+            arr.push(obj2)
+            // data.innerText="row"+i+"x"+j
+            data.addEventListener("click", checkTd);
+
+
+            row.appendChild(data);
+
+        }
+
+
+        table.appendChild(row);
 
     }
-
-    for (let j = 0; j < 5; j++) {
-
-        let data = document.createElement("td");
-        data.setAttribute("id", i + "x" + j);
-        let obj2 = { "id": i + "x" + j, "checkTest": 0 }
-        data.innerText = i + "x" + j
-        arr.push(obj2)
-        // data.innerText="row"+i+"x"+j
-        data.addEventListener("click", checkTd);
-
-
-        row.appendChild(data);
-
-    }
-
-
-    table.appendChild(row);
-
 }
-
-boardContainer.appendChild(table)
-
-
 
 boardContainer.appendChild(table)
 
@@ -144,68 +256,13 @@ function checkTd() {
 
     test(elementPos)
 
-    console.log(arr)
 }
 
 function test(index) {
-    //   var answers =[  {"id": "0x0", "checkTest": 0},
-    //  {"id": "0x1", "checkTest": 0},
-    // {"id": "0x2", "checkTest": 1},
-    //  {"id": "0x3", "checkTest": 0},
-    //  {"id": "0x4", "checkTest": 0},
-    //  {"id": "1x0", "checkTest": 0},
-    //  {"id": "1x1", "checkTest": 1},{"id": "1x2", "checkTest": 1},
-    //  {"id": "1x3", "checkTest": 1},
-    //  {"id": "1x4", "checkTest": 0},
-    //  {"id": "2x0", "checkTest": 1},
-    // {"id": "2x1", "checkTest": 0},
-    // {"id": "2x2", "checkTest": 1},
-    // {"id": "2x3", "checkTest": 0},
-    // {"id": "2x4", "checkTest": 1},
-    // {"id": "3x0", "checkTest": 0},
-    // {"id": "3x1", "checkTest": 0},
-    // {"id": "3x2", "checkTest": 1},
-    // {"id": "3x3", "checkTest": 0},
-    // {"id": "3x4", "checkTest": 0},
-    // {"id": "4x0", "checkTest": 0},
-    // {"id": "4x1", "checkTest": 0},
-    // {"id": "4x2", "checkTest": 1},
-    // {"id": "4x3", "checkTest": 0},
-    // {"id": "4x4", "checkTest": 0}]
 
-
-    // for(let i=0;i<answers.length;i++){
-
-    //     if(answers[i]!=arr[i]){
-    //         console.log("")
-    //     }
-
-    // }
-
-
-    // var indexArr=[2,6,7,8,10,12,14,17,22]
-
-    // for(let i = 0;i<arr.length;i++){
-
-    //     if(indexArr.includes(i)){
-    //         console.log("YES")
-    //     }
-
-    // }
-
-
-    //     if(JSON.stringify(answers) == JSON.stringify(arr) ){
-    //         console.log("YOU WIN !!!!!!")
-    //     }else{
-    //         console.log("Just another test")
-    //     }
-
-
-
-
-    // var indexArr=[2,6,7,8,10,12,14,17,22]
-    var indexArr = [1, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 22]
-
+    // var indexArr=[2,6,7,8,10,12,14,17,22] //arrows
+    //var indexArr = [1, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 22]//heart
+    var indexArr = [3,8,9,10,11,12,14,15,16,17,18,19,20,24,31,38,44,45]//umbrella
     //for(let i = 0;i<arr.length;i++){
 
     if (indexArr.includes(index) && arr[index].checkTest == 1) {
@@ -222,11 +279,6 @@ function test(index) {
         counter--
     }
 
-    //}
-
-
-
-    console.log("counter is : " + counter)
 
 
 }
