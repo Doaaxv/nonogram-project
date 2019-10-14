38f1bb05126421aef5a721ee0e341d5cabc89711
Doaa AlHajjaji <Doaa.mh.uni@gmail.com>
2019-10-14 11:23:41 +0300

    stored grid in object and made start page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,28 @@
 var boardContainer = document.getElementById("boardContainer")
+var menu = document.getElementById("menu")
 
-let table = document.createElement("table")
+var table = document.createElement("table")
 table.setAttribute("id", "table")
 
-let counter = 0
+var counter = 0
 var arr = []
-//heart
-// var arrThHorizontal = ["2,1", "4", "4", "4", "2"]
-// var arrThVertical = ["1,1,1", "5", "5", "3", "1"]
-// var rows = 5
-//arrow
-// var arrThHorizontal = [ "1","1","5","1","1"]
-// var arrThVertical = ["1","3","1,1,1,","1","1"]
-// var rows = 5
-//umbrella
-var arrThHorizontal = ["1","2","2,1","7","2","2","1"]
-var arrThVertical = ["1","5","7","1","1","1","2"]
-var rows = 7
+var level = 0
+
+
+let levels = [{"arrThHorizontal":["2", "4", "4", "4", "2"],
+"arrThVertical":["1,1", "5", "5", "3", "1"],
+"answers":[1, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 22],"rows":5,"shape":"Heart"},
+{"arrThHorizontal":[ "1","1","5","1","1"],"arrThVertical": ["1","3","1,1,1,","1","1"],
+"answers":[2,6,7,8,10,12,14,17,22],"rows":5,"shape":"arrow"},
+{"arrThHorizontal": ["1", "2", "2,1", "7", "2", "2", "1"],"arrThVertical":["1", "5", "7", "1", "1", "1", "2"],
+"answers":[3, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 24, 31, 38, 44, 45],
+"rows":7,"shape":"umbrella"}]
+
+
+function makeGrid(){
+    boardContainer.innerHTML = " "
+
+}
 
 
 function longestElement(array) {
@@ -31,18 +37,19 @@ function longestElement(array) {
 }
 
 //vertical 
-var longest_vertical_element = longestElement(arrThVertical)
+var longest_vertical_element = longestElement(levels[level].arrThVertical)
 let longest_element_vertical = longest_vertical_element.split(",")
 
 //horizontal
-var longest_horizontal_element = longestElement(arrThHorizontal)
+var longest_horizontal_element = longestElement(levels[level].arrThHorizontal)
 let longest_element_horizontal = longest_horizontal_element.split(",")
 
-if(longest_element_vertical!=0){
-for (let i = 0; i < longest_element_vertical.length; i++) {
-    arrThHorizontal.unshift(" ")
-}}else{
-    arrThHorizontal.unshift(" ") 
+if (longest_element_vertical != 0) {
+    for (let i = 0; i < longest_element_vertical.length; i++) {
+        levels[level].arrThHorizontal.unshift(" ")
+    }
+} else {
+    levels[level].arrThHorizontal.unshift(" ")
 }
 
 
@@ -54,70 +61,70 @@ if (longest_element_horizontal != 0) {
 
 
 
-if(cols != 1){
-for (let i = 0; i < cols; i++) {
-    let thead_horizontal = document.createElement("thead")
-    let tr_horizontal = document.createElement("tr")
-    for (let a = 0; a < arrThHorizontal.length; a++) {
-        if (arrThHorizontal[a].length > 1) {
-            var bigOne = arrThHorizontal[a].split(",")
-            var difference_hori = 0
-            if (bigOne.length < cols) {
-                difference_hori = cols - bigOne.length
-                if (i >= difference_hori) {
-                    let th_horizontal = document.createElement("th");
-                    th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
-                    th_horizontal.setAttribute("class", "thHorizontal")
-                    th_horizontal.innerText = bigOne[i - 1]
-                    tr_horizontal.appendChild(th_horizontal)
+if (cols != 1) {
+    for (let i = 0; i < cols; i++) {
+        let thead_horizontal = document.createElement("thead")
+        let tr_horizontal = document.createElement("tr")
+        for (let a = 0; a < levels[level].arrThHorizontal.length; a++) {
+            if (levels[level].arrThHorizontal[a].length > 1) {
+                var bigOne = levels[level].arrThHorizontal[a].split(",")
+                var difference_hori = 0
+                if (bigOne.length < cols) {
+                    difference_hori = cols - bigOne.length
+                    if (i >= difference_hori) {
+                        let th_horizontal = document.createElement("th");
+                        th_horizontal.setAttribute("id", "thH" + levels[level].arrThHorizontal[a])
+                        th_horizontal.setAttribute("class", "thHorizontal")
+                        th_horizontal.innerText = bigOne[i - 1]
+                        tr_horizontal.appendChild(th_horizontal)
+                    } else {
+                        let th_horizontal = document.createElement("th");
+                        th_horizontal.setAttribute("id", "thH" + levels[level].arrThHorizontal[a])
+                        th_horizontal.setAttribute("class", "thHorizontal")
+                        th_horizontal.innerText = " "
+                        tr_horizontal.appendChild(th_horizontal)
+                    }
+
                 } else {
                     let th_horizontal = document.createElement("th");
-                    th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
+                    th_horizontal.setAttribute("id", "thH" + levels[level].arrThHorizontal[a])
                     th_horizontal.setAttribute("class", "thHorizontal")
-                    th_horizontal.innerText = " "
+                    th_horizontal.innerText = bigOne[i]
                     tr_horizontal.appendChild(th_horizontal)
                 }
 
+
             } else {
-                let th_horizontal = document.createElement("th");
-                th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
-                th_horizontal.setAttribute("class", "thHorizontal")
-                th_horizontal.innerText = bigOne[i]
-                tr_horizontal.appendChild(th_horizontal)
+                if (i > 0) {
+                    let th_horizontal = document.createElement("th");
+                    th_horizontal.setAttribute("id", "thH" + levels[level].arrThHorizontal[a])
+                    th_horizontal.setAttribute("class", "thHorizontal")
+                    th_horizontal.innerText = levels[level].arrThHorizontal[a]
+                    tr_horizontal.appendChild(th_horizontal)
+                } else {
+                    let th_horizontal = document.createElement("th");
+                    th_horizontal.setAttribute("id", "thH" + levels[level].arrThHorizontal[a])
+                    th_horizontal.setAttribute("class", "thHorizontal")
+                    th_horizontal.innerText = " "
+                    tr_horizontal.appendChild(th_horizontal)
+
+                }
             }
 
-
-        } else {
-            if (i > 0) {
-                let th_horizontal = document.createElement("th");
-                th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
-                th_horizontal.setAttribute("class", "thHorizontal")
-                th_horizontal.innerText = arrThHorizontal[a]
-                tr_horizontal.appendChild(th_horizontal)
-            } else {
-                let th_horizontal = document.createElement("th");
-                th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
-                th_horizontal.setAttribute("class", "thHorizontal")
-                th_horizontal.innerText = " "
-                tr_horizontal.appendChild(th_horizontal)
-
-            }
         }
 
+        thead_horizontal.appendChild(tr_horizontal)
+        table.appendChild(thead_horizontal)
     }
-    
-    thead_horizontal.appendChild(tr_horizontal)
-    table.appendChild(thead_horizontal)
-}
-}else{
+} else {
     let thead_horizontal = document.createElement("thead")
     let tr_horizontal = document.createElement("tr")
-    for (let a = 0; a < arrThHorizontal.length; a++) {
+    for (let a = 0; a < levels[level].arrThHorizontal.length; a++) {
         let th_horizontal = document.createElement("th");
-        th_horizontal.setAttribute("id", "thH" + arrThHorizontal[a])
+        th_horizontal.setAttribute("id", "thH" + levels[level].arrThHorizontal[a])
         th_horizontal.setAttribute("class", "thHorizontal")
-        th_horizontal.innerText = arrThHorizontal[a]
-        tr_horizontal.appendChild(th_horizontal) 
+        th_horizontal.innerText = levels[level].arrThHorizontal[a]
+        tr_horizontal.appendChild(th_horizontal)
     }
     thead_horizontal.appendChild(tr_horizontal)
     table.appendChild(thead_horizontal)
@@ -126,30 +133,22 @@ for (let i = 0; i < cols; i++) {
 
 
 
-
-
-function createVerticalHeading(i, col, txt, thVert) {
-    // if (col == 0) {
-    thVert.setAttribute("id", "thV" + arrThVertical[i])
+function createVerticalHeading(i, txt, thVert) {
+    thVert.setAttribute("id", "thV" + levels[level].arrThVertical[i])
     thVert.setAttribute("class", "verticalth")
     thVert.innerText = txt
-    // } else {
-    //     thVert.setAttribute("id", "thV" + arrThVertical[i])
-    //     thVert.setAttribute("class", "verticalth")
-    //     thVert.innerText = txt
-    // }
-
 
 }
 
 
-for (let i = 0; i < rows; i++) {
+for (let i = 0; i < levels[level].rows; i++) {
     let row = document.createElement("tr");
     row.setAttribute("id", "row" + i);
-    let el_length = arrThVertical[i].split(",");
+    let el_length = levels[level].arrThVertical[i].split(",");
+    
 
     if (longest_element_vertical != 0) {
-        if (arrThVertical[i].length > 1) {
+        if (levels[level].arrThVertical[i].length > 1) {
 
             let forLength = longest_element_vertical.length
 
@@ -162,38 +161,38 @@ for (let i = 0; i < rows; i++) {
                 if (diffrence != 0) {
                     if (z >= diffrence) {
                         let thVert = document.createElement("th");
-                        createVerticalHeading(i, 0, el_length[z - 1], thVert)
+                        createVerticalHeading(i,el_length[z - 1], thVert)
                         row.appendChild(thVert)
                     } else {
                         let thVert = document.createElement("th");
-                        createVerticalHeading(i, 0, " ", thVert)
+                        createVerticalHeading(i," ", thVert)
                         row.appendChild(thVert)
                     }
 
                 } else {
 
                     let thVert = document.createElement("th");
-                    createVerticalHeading(i, 0, el_length[z], thVert)
+                    createVerticalHeading(i,el_length[z], thVert)
                     row.appendChild(thVert)
                 }
             }
         } else {
             for (let z = 0; z < longest_element_vertical.length - 1; z++) {
                 let thVert = document.createElement("th");
-                createVerticalHeading(i, 0, " ", thVert)
+                createVerticalHeading(i," ", thVert)
                 row.appendChild(thVert)
             }
             let thVert = document.createElement("th");
-            createVerticalHeading(i, longest_element_vertical.length, arrThVertical[i], thVert)
+            createVerticalHeading(i, levels[level].arrThVertical[i], thVert)
             row.appendChild(thVert)
 
         }
 
-        for (let j = 0; j < rows; j++) {
+        for (let j = 0; j < levels[level].rows; j++) {
             let data = document.createElement("td");
             data.setAttribute("id", i + "x" + j);
             let obj2 = { "id": i + "x" + j, "checkTest": 0 }
-            data.innerText = i + "x" + j
+            // data.innerText = i + "x" + j
             arr.push(obj2)
             // data.innerText="row"+i+"x"+j
             data.addEventListener("click", checkTd);
@@ -206,19 +205,19 @@ for (let i = 0; i < rows; i++) {
 
         table.appendChild(row);
 
-    }else{
+    } else {
 
-            let thVert = document.createElement("th");
-            createVerticalHeading(i, longest_element_vertical.length, arrThVertical[i], thVert)
-            row.appendChild(thVert)
-        
+        let thVert = document.createElement("th");
+        createVerticalHeading(i,levels[level].arrThVertical[i], thVert)
+        row.appendChild(thVert)
 
 
-        for (let j = 0; j < rows; j++) {
+
+        for (let j = 0; j < levels[level].rows; j++) {
             let data = document.createElement("td");
             data.setAttribute("id", i + "x" + j);
             let obj2 = { "id": i + "x" + j, "checkTest": 0 }
-            data.innerText = i + "x" + j
+            //data.innerText = i + "x" + j
             arr.push(obj2)
             // data.innerText="row"+i+"x"+j
             data.addEventListener("click", checkTd);
@@ -235,6 +234,9 @@ for (let i = 0; i < rows; i++) {
 }
 
 boardContainer.appendChild(table)
+
+
+
 
 
 function checkTd() {
@@ -260,20 +262,18 @@ function checkTd() {
 
 function test(index) {
 
-    // var indexArr=[2,6,7,8,10,12,14,17,22] //arrows
-    //var indexArr = [1, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 22]//heart
-    var indexArr = [3,8,9,10,11,12,14,15,16,17,18,19,20,24,31,38,44,45]//umbrella
-    //for(let i = 0;i<arr.length;i++){
 
-    if (indexArr.includes(index) && arr[index].checkTest == 1) {
-        if (counter == indexArr.length - 1) {
+    if (levels[level].answers.includes(index) && arr[index].checkTest == 1) {
+        if (counter == levels[level].answers.length - 1) {
             console.log("WIN !!!")
+            nextBtn.style.display = "block"
+            level +=1
         }
         counter++
     }
 
-    if (indexArr.includes(index) && arr[index].checkTest == 2) {
-        if (counter == indexArr.length - 1) {
+    if (levels[level].answers.includes(index) && arr[index].checkTest == 2) {
+        if (counter == levels[level].answers.length - 1) {
             console.log("nops")
         }
         counter--
@@ -282,3 +282,82 @@ function test(index) {
 
 
 }
+
+///////////////////////Buttons ////////////////////////////
+var btnInst = document.createElement("button")
+btnInst.setAttribute("id","btnInst")
+btnInst.innerText = "Game Instructions"
+
+ //document.getElementById("btnInst")
+
+/////////////////////// start game button /////////////////////// 
+let btnStart = document.createElement("button")
+btnStart.setAttribute("id","btnStart")
+btnStart.innerText="Start Game"
+menu.appendChild(btnStart)
+menu.appendChild(btnInst)
+//document.getElementById("btnStart")
+
+ btnStart.addEventListener("click",function(){
+    counter = 0
+    level = 0
+    menu.style.display = "none"
+    boardContainer.style.display="block"
+})
+
+/////////////////////// instruction button /////////////////////// 
+let $modaBody = document.querySelector(".model-body")
+let $model = document.getElementById("myModal");
+btnInst.addEventListener("click", function () {
+
+    let $close = document.querySelector(".close")
+
+
+    let htmlContent = " <p> Nonogram, also known as Picross or Griddlers,"+
+    " is a puzzle game in which you fill in pixels to complete a picture.</p> "
+
+    htmlContent += "<img src ='assets/img/nonogrampic.png' />"
+
+    $modaBody.innerHTML = htmlContent;
+    $model.style.display = "block"
+    $close.addEventListener("click", function () {
+        $model.style.display = "none"
+    });
+
+
+})
+/////////////////////// reset button ////////////////
+var resetBtn = document.createElement("button")
+resetBtn.innerText = "Reset"
+resetBtn.addEventListener("click",function(){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].checkTest!= 0){
+            arr[i].checkTest= 0
+            let data = document.getElementById(arr[i].id)
+            data.style.backgroundColor = "white"
+            counter = 0
+
+        }
+    }
+    
+})
+
+boardContainer.appendChild(resetBtn)
+/////////////////////// back to menu button ////////////////
+var menuBtn = document.createElement("button")
+menuBtn.innerText = "Menu"
+menuBtn.addEventListener("click",function(){
+    menu.style.display = "block"
+    boardContainer.style.display="none"
+    
+})
+boardContainer.appendChild(menuBtn)
+/////////////////////// next level button ////////////////
+var nextBtn = document.createElement("button")
+nextBtn.innerText = "Next level"
+nextBtn.style.display = "none"
+nextBtn.addEventListener("click",function(){
+    level = 1
+    makeGrid()
+})
+boardContainer.appendChild(nextBtn)
