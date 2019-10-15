a95d0b22dfdadc67db9c90b0b4ee95ca8b4f25ca
Doaa AlHajjaji <Doaa.mh.uni@gmail.com>
2019-10-15 16:57:20 +0300

    fixed game logic added new popups

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,8 @@ var table = document.createElement("table")
 var heading = document.querySelector("h1")
 table.setAttribute("id", "table")
 
+
+
 var counter = 0
 var arr = []
 var level = 0
@@ -20,11 +22,7 @@ let levels = [
     {
         "arrThHorizontal": ["2", "4", "4", "4", "2"],
         "arrThVertical": ["1,1", "5", "5", "3", "1"],
-        "answers": [1, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 22], "rows": 5, "shape": "Heart"
-    },
-    {
-        "arrThHorizontal": ["1", "1", "5", "1", "1"], "arrThVertical": ["1", "3", "1,1,1", "1", "1"],
-        "answers": [2, 6, 7, 8, 10, 12, 14, 17, 22], "rows": 5, "shape": "arrow"
+        "answers": [1, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 22], "rows": 5, "shape": "Heart"
     },
     {
         "arrThHorizontal": ["1", "2", "2,1", "7", "2", "2", "1"], "arrThVertical": ["1", "5", "7", "1", "1", "1", "2"],
@@ -32,6 +30,11 @@ let levels = [
         "rows": 7, "shape": "umbrella"
     }]
 
+/*
+{
+    "arrThHorizontal": ["1", "1", "5", "1", "1"], "arrThVertical": ["1", "3", "1,1,1", "1", "1"],
+    "answers": [2, 6, 7, 8, 10, 12, 14, 17, 22], "rows": 5, "shape": "arrow"
+}, */
 
 
 function makeGrid() {
@@ -183,7 +186,7 @@ function makeGrid() {
                     let data = document.createElement("td");
                     data.setAttribute("id", i + "x" + j);
                     let obj2 = { "id": i + "x" + j, "checkTest": 0 }
-                    // data.innerText = i + "x" + j
+                    //data.innerText = i + "x" + j
                     arr.push(obj2)
                     // data.innerText="row"+i+"x"+j
                     data.addEventListener("click", checkTd);
@@ -236,8 +239,19 @@ function makeGrid() {
 
     } else {
         console.log("Game Ended")
-        boardContainer.innerHTML = "<p>Game Ended</p>"
+        //boardContainer.innerHTML = "<p>Game Ended</p>"
         gameBtns.innerHTML = " "
+
+        Swal.fire({
+            background: '#e2df45 url(http://www.graphicartsunit.com/images/noise.png)',
+            showConfirmButton: false,
+            showCloseButton: true,
+            title: "Game Ended"
+        })
+        heading.style.display = "block"
+        menu.style.display = "block"
+
+
     }
 }/// end of function 
 
@@ -262,15 +276,15 @@ function clearGrid() {
     for (let j = 0; j < levels.length; j++) {
         console.log("before the splice" + levels[j].arrThHorizontal)
         for (let i = 0; i < levels[j].arrThHorizontal.length; i++) {
-            levels[j].arrThHorizontal.forEach(function(item, index){
-                if(levels[j].arrThHorizontal[index]==""){
-                    levels[j].arrThHorizontal.splice(index, 1) 
+            levels[j].arrThHorizontal.forEach(function (item, index) {
+                if (levels[j].arrThHorizontal[index] == "") {
+                    levels[j].arrThHorizontal.splice(index, 1)
                 }
 
             })
+        }
+        console.log("After splice: " + levels[j].arrThHorizontal)
     }
-    console.log("After splice: " + levels[j].arrThHorizontal)
-}
 
 }
 
@@ -299,11 +313,14 @@ function checkTd() {
     var elementPos = arr.map(function (x) { return x.id; }).indexOf(this.id);
     var objectFound = arr[elementPos];
 
+    this.innerText = " "
+
     if (objectFound.checkTest == 0) {
-        this.style.backgroundColor = "blue";
+        this.style.backgroundColor = "darkslategrey";
         arr[elementPos].checkTest = 1;
     } else if (objectFound.checkTest == 1) {
-        this.style.backgroundColor = "yellow";
+        this.style.backgroundColor = "white";
+        this.innerText = "X"
         arr[elementPos].checkTest = 2
     } else if (objectFound.checkTest == 2) {
         this.style.backgroundColor = "white";
@@ -315,10 +332,64 @@ function checkTd() {
 }
 
 function test(index) {
-
-
+    console.log("inside test and counter is: "+counter)
     if (levels[level].answers.includes(index) && arr[index].checkTest == 1) {
-        if (counter == levels[level].answers.length - 1) {
+        counter++
+        console.log(levels[level].answers.length + " counter:" + counter)
+        if (counter == levels[level].answers.length) {
+
+            let just_checking = 0
+
+
+            for (let k = 0; k < arr.length; k++) {
+                if ((levels[level].answers.includes(k) == false) && arr[k].checkTest == 1) {
+                    just_checking = 1
+                    console.log(counter)
+                    console.log(k)
+                    console.log("WRONGGGG")
+                }
+            }
+
+            if (just_checking == 0) {
+                console.log("WIN !!!")
+                nextBtn.style.display = "inline"
+
+                for (let i = 0; i < arr.length; i++) {
+                    let data = document.getElementById(arr[i].id);
+                    console.log(arr[i].id + " onclick stops")
+                    data.removeEventListener("click", checkTd)
+                }
+
+                Swal.fire({
+                    background: '#e8afbb url(http://www.graphicartsunit.com/images/noise.png)',
+                    imageUrl: 'assets/img/' + level + '.png',
+                    imageWidth: 300,
+                    showConfirmButton: false,
+                    showCloseButton: true,
+                    text: levels[level].shape
+                })
+            }
+
+        }
+
+    }
+
+    if (levels[level].answers.includes(index) && arr[index].checkTest == 2) {
+        counter--
+    }
+
+    if (counter == levels[level].answers.length) {
+        console.log("counter == levels[level].answers.length")
+        var check_arr = 0
+        for (let i = 0; i < arr.length; i++) {
+            if (!(levels[level].answers.includes(i)) && arr[i].checkTest == 1) {
+                console.log("NOPE")
+                check_arr = 1
+            }
+        }
+        if (check_arr == 0) {
+            console.log("check_arr ==0")
+            //WIN !!!
             console.log("WIN !!!")
             nextBtn.style.display = "inline"
 
@@ -328,15 +399,15 @@ function test(index) {
                 data.removeEventListener("click", checkTd)
             }
 
+            Swal.fire({
+                background: '#e8afbb url(http://www.graphicartsunit.com/images/noise.png)',
+                imageUrl: 'assets/img/' + level + '.png',
+                imageWidth: 300,
+                showConfirmButton: false,
+                showCloseButton: true,
+                text: levels[level].shape
+            })
         }
-        counter++
-    }
-
-    if (levels[level].answers.includes(index) && arr[index].checkTest == 2) {
-        if (counter == levels[level].answers.length - 1) {
-            console.log("nops")
-        }
-        counter--
     }
 
 
@@ -373,19 +444,33 @@ let $modaBody = document.querySelector(".model-body")
 let $model = document.getElementById("myModal");
 btnInst.addEventListener("click", function () {
 
-    let $close = document.querySelector(".close")
+    Swal.fire({
+        background: '#e8afbb url(http://www.graphicartsunit.com/images/noise.png)',
+        imageUrl: 'assets/img/nonogrampic.png',
+        imageWidth: 400,
+        showConfirmButton: false,
+        showCloseButton: true,
+        text: "Nonogram, also known as Picross or Griddlers," +
+            " is a puzzle game in which you fill in pixels to complete a picture"
+    })
+    // background: '#fff url(/images/trees.png)'
+
+    // "<img id = 'imgThing' src ='assets/img/nonogrampic.png' />",
+    // "TITLE!!!")
+
+    // let $close = document.querySelector(".close")
 
 
-    let htmlContent = " <p> Nonogram, also known as Picross or Griddlers," +
-        " is a puzzle game in which you fill in pixels to complete a picture.</p> "
+    // let htmlContent = " <p> Nonogram, also known as Picross or Griddlers," +
+    //     " is a puzzle game in which you fill in pixels to complete a picture.</p> "
 
-    htmlContent += "<img src ='assets/img/nonogrampic.png' />"
+    // htmlContent += "<img src ='assets/img/nonogrampic.png' />"
 
-    $modaBody.innerHTML = htmlContent;
-    $model.style.display = "block"
-    $close.addEventListener("click", function () {
-        $model.style.display = "none"
-    });
+    // $modaBody.innerHTML = htmlContent;
+    // $model.style.display = "block"
+    // $close.addEventListener("click", function () {
+    //     $model.style.display = "none"
+    // });
 
 
 })
@@ -431,7 +516,7 @@ function createBtns() {
     nextBtn = document.createElement("a")
     nextBtn.setAttribute("href", "#")
     nextBtn.setAttribute("class", "btn")
-    nextBtn.innerText = "Next level"
+    nextBtn.innerText = "Next"
     nextBtn.style.display = "none"
     nextBtn.addEventListener("click", function () {
         level += 1
@@ -447,20 +532,80 @@ function createBtns() {
     hintBtn.addEventListener("click", function () {
         console.log("randomize hint")
         let randomNum = Math.floor(Math.random() * arr.length)
+        //element.classList.contains(class);
 
         console.log(randomNum)
-        // console.log("color this blue")
 
-        while (levels[level].answers.includes(randomNum) && arr[randomNum].checkTest == 1) {
-            randomNum = Math.floor(Math.random() * arr.length)
+        var flash_flag = 0;
+        var con1 = (arr[randomNum].checkTest == 0 || arr[randomNum].checkTest == 2) && (levels[level].answers.includes(randomNum))
+        var con2 = arr[randomNum].checkTest == 1 && !(levels[level].answers.includes(randomNum))
+        let data = document.getElementById(arr[randomNum].id)
+        // if(con1 == true || con2 == true){
+        //     flash_flag = 1
+        // }
+
+        //var con1 = arr[randomNum].checkTest == 1 && (levels[level].answers.includes(randomNum)
+
+        // var con3 = arr[randomNum].checkTest == 0 && !(levels[level].answers.includes(randomNum))
+        // var con4 = arr[randomNum].checkTest == 1 && levels[level].answers.includes(randomNum)
+
+        while (flash_flag == 0) {
+            if ((con1 == true || con2 == true)) {
+                randomNum = Math.floor(Math.random() * arr.length)
+                con1 = (arr[randomNum].checkTest == 0 || arr[randomNum].checkTest == 2) && (levels[level].answers.includes(randomNum))
+                con2 = arr[randomNum].checkTest == 1 && !(levels[level].answers.includes(randomNum))
+                console.log("inside if of flash_flag")
+            } else {
+                data.classList.remove('animated');
+                data.classList.remove('flash');
+                flash_flag = 1
+            }
+
         }
 
-        if (arr[randomNum].checkTest == 1 && !(levels[level].answers.includes(randomNum))) {
-            // arr[randomNum].checkTest =
-            //this.style.backgroundColor = "blue";
-            let data = document.getElementById(arr[randomNum].id)
-            data.style.backgroundColor = "pink"
+        if (flash_flag == 1) {
+
+            data.classList.add('animated', 'flash')
         }
+
+        //     console.log(randomNum)
+
+
+
+        // while(flash_flag == 0){
+        //     if(con1 == true || con2 == true){
+        //     randomNum = Math.floor(Math.random() * arr.length)
+        //     flash_flag =1
+        // }
+        // }
+
+        // data.classList.add('animated', 'flash')
+
+        // while (levels[level].answers.includes(randomNum) && arr[randomNum].checkTest == 1) {
+        //     randomNum = Math.floor(Math.random() * arr.length)
+        // }
+
+        // console.log(randomNum+" after first while")
+        // var con = (arr[randomNum].checkTest == 0) && (levels[level].answers.includes(randomNum)) // should be colored
+        // var con2 = arr[randomNum].checkTest == 1 && !(levels[level].answers.includes(randomNum))
+
+        // var flag = 0
+
+        // while(flag == 0){
+
+        //     if (con == true || con2 == true) {
+        //         console.log(con+" if")
+        //         let data = document.getElementById(arr[randomNum].id)
+
+        //         data.classList.add('animated', 'flash')
+        //         flag = 1
+        //     }
+
+        //     randomNum = Math.floor(Math.random() * arr.length)
+
+        // }
+
+
 
 
     })
@@ -474,7 +619,13 @@ function createBtns() {
  * https://www.bestcssbuttongenerator.com/#/26
  * https://uicookies.com/css-buttons/
  *
- * 
+ * https://sweetalert2.github.io/
+ *
+ * animation:
+ * https://daneden.github.io/animate.css/
+ * https://github.com/daneden/animate.css/
+ *
+ *
  * https://css-tricks.com/adding-stroke-to-web-text/
  * flickering
  * https://stackoverflow.com/questions/8360130/how-to-make-a-text-flash-in-html-javascript
